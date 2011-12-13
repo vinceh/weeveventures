07c6e989f8476e4a0403387ceecc2d75590c7c28
Alexey Suhih <lexerom@gmail.com>
2011-12-13 15:51:30 +0700

    design fixes

---
+++ www/sites/all/modules/weeve/project/tpl/terms_form.tpl.php
@@ -0,0 +1,21 @@
+<div class='signup-header'>
+  <div class='signup-title'>Starting a project - Guidelines & Conditions</div>
+  <div class='signup-subtitle'>Review our conditions to make sure your project is eligible</div>
+</div>
+
+<div class='project-signup-terms-wrapper'>
+
+  <div class='signup-content'>Weeve is a funding platform focused on a broad spectrum of non-profit projects. The guidelines below articulate our mission and focus. Please note that any project that violates these guidelines will be declined or removed. Please contact us if you have any questions.</div>
+
+  <div class='signup-content'>Review each guideline below to confirm that your project meets it:</div>
+  <ul>
+    <li>I am creating a project. Weeve is for the funding of projects that have clearly defined goals and expectations.</li>
+    <li>My project fits within one of Kickstarter's categories. Kickstarter can be used to fund projects from the creative fields of Art, Comics, Dance, Design, Fashion, Film, Food, Games, Music, Photography, Publishing, Technology, and Theater. We currently only support projects from these categories.</li>
+    <li>My project does not incorporate charity or cause funding. Examples of prohibited use include raising money for the Red Cross, funding an awareness campaign, funding a scholarship, or donating a portion of funds raised on Kickstarter to a charity or cause.</li>
+
+    <li>My project is not a "fund my life" project. Examples include projects to pay tuition or bills, go on vacation, or buy a new camera.</li>
+    <li>My project offers rewards, not financial incentives. The Kickstarter economy is based on the offering of rewards – copies of the work, limited editions, fun experiences. Offering financial incentives, such as ownership, financial returns (for example, a share of profits), or repayment (loans) is prohibited.</li>
+  </ul>
+</div>
+
+<?= drupal_render($form) ?>
