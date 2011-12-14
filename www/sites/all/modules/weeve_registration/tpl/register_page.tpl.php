965de5ecb49cb7744e25cc6c9807978257dadad8
Nikolay <nikolas.shkuro@gmail.com>
2011-12-14 15:49:46 +0700

    ISSUE-2: * New landing signup page

--- www/sites/all/modules/weeve_registration/tpl/register_page.tpl.php
+++ www/sites/all/modules/weeve_registration/tpl/register_page.tpl.php
@@ -5,18 +5,29 @@
 </div>
 
 
-<div class='signup-form'>
-
-	<div class='signup-role-wrap'>
-		<div class='signup-role'><?php print l(t('I\'m a user!'), 'signup/user'); ?></div>
-		<div class='signup-role-hint'><?php print t('Join the Community'); ?></div>
+<div class='landingusersspace'>
+	<div class='usericon'></div>
+	<div class='userspacewrap'>
+		<div class='landingsentencetitle weevetitle'><?php print t('Are you a donor?'); ?></div>
+		<div class='landingsentence weevetext'><?php print t('Discover, fund, and earn your way through social empathy!'); ?></div>
 	</div>
-
-	<div class='signup-role-wrap'>
-		<div class='signup-role'><?php print l(t('I\'m a non-profit!'), 'signup/npo'); ?></div>
-		<div class='signup-role-hint'><?php print t('Build the Community'); ?></div>
+	<a class='weeve-medium-button landingbutton' href='<?php print url('signup/user'); ?>'><?php print t('Sign up'); ?></a>
+</div>
+<div class='landingusersspace'>
+	<div class='usericon'></div>
+	<div class='userspacewrap'>
+		<div class='landingsentencetitle weevetitle'><?php print t('Are you a non-profit organization?'); ?></div>
+		<div class='landingsentence weevetext'><?php print t('Claim your page and start creating projects!'); ?></div>
 	</div>
-
+	<a class='weeve-medium-button landingbutton' href='<?php print url('signup/npo'); ?>'><?php print t('Claim your page'); ?></a>
+</div>
+<div class='landingusersspace'>
+	<div class='usericon'></div>
+	<div class='userspacewrap'>
+		<div class='landingsentencetitle weevetitle'><?php print t('Are you a company?'); ?></div>
+		<div class='landingsentence weevetext'><?php print t('Claim your page and create your corporate image!'); ?></div>
+	</div>
+	<a class='weeve-medium-button landingbutton' href='<?php print url('signup/corp'); ?>'><?php print t('Claim your page'); ?></a>
 </div>
 
 <div class='footer'></div>
