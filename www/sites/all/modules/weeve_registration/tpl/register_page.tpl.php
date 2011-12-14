5fb97364398ef294b7adefc08cf3b805ed328fc6
Nikolay <nikolas.shkuro@gmail.com>
2011-12-14 15:10:10 +0700

    ISSUE-2: * Theming register pages

--- www/sites/all/modules/weeve_registration/tpl/register_page.tpl.php
+++ www/sites/all/modules/weeve_registration/tpl/register_page.tpl.php
@@ -1,2 +1,22 @@
-<div><?php print l('User', 'signup/user'); ?></div>
-<div><?php print l('NPO', 'signup/npo'); ?></div>
+
+<div class='signup-header'>
+	<div class='signup-title'><?php print t('Join Weeve'); ?></div>
+	<div class='signup-subtitle'><?php print t('choose your role'); ?></div>
+</div>
+
+
+<div class='signup-form'>
+
+	<div class='signup-role-wrap'>
+		<div class='signup-role'><?php print l(t('I\'m a user!'), 'signup/user'); ?></div>
+		<div class='signup-role-hint'><?php print t('Join the Community'); ?></div>
+	</div>
+
+	<div class='signup-role-wrap'>
+		<div class='signup-role'><?php print l(t('I\'m a non-profit!'), 'signup/npo'); ?></div>
+		<div class='signup-role-hint'><?php print t('Build the Community'); ?></div>
+	</div>
+
+</div>
+
+<div class='footer'></div>
