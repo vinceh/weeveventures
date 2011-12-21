db63195543d7274fedef784e75df3ffa36fb3ba0
Alexey Suhih <lexerom@gmail.com>
2011-12-21 17:40:56 +0700

    npo projects + youtube video autoupload + style

--- www/sites/all/themes/weeve/node-project.tpl.php
+++ www/sites/all/themes/weeve/node-project.tpl.php
@@ -1,7 +1,14 @@
 <div id="node-<?php print $node->nid; ?>" class="project node<?php if ($sticky) { print ' sticky'; } ?><?php if (!$status) { print ' node-unpublished'; } ?>">
 
   <div class="project-image">
+
+<?php
+  if ($node->field_project_video[0]['video_id']):
+   print theme('video_upload_video', $node->field_project_video[0]['video_id'], 580, 460);
+  else:
+?>
 <?php print theme_image($node->field_project_image[0]['filepath']); ?>
+<?php endif; ?>
   </div>
   <div class="project-body">
 <?php print $node->native_body ?>
