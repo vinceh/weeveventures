37ffe7f995f44369f1e847db2e7c0b68b35e57d6
Nikolay <nikolas.shkuro@gmail.com>
2011-12-27 14:53:57 +0700

    FIX uploader

--- www/weeve_upload.php
+++ www/weeve_upload.php
@@ -48,14 +48,14 @@ if ($auth) {
   $callback = $_GET['CKEditorFuncNum'];
   $file_name = $_FILES['upload']['name'];
   $file_name_tmp = $_FILES['upload']['tmp_name'];
-  $file_new_name = trim($_SERVER['DOCUMENT_ROOT'], '/') . '/sites/default/files/npo_projects/' . $user->uid . '/files/';
+  $file_new_name = rtrim($_SERVER['DOCUMENT_ROOT'], '/') . '/sites/default/files/npo_projects/' . $user->uid . '/files/';
 
-  if (!is_dir(trim($_SERVER['DOCUMENT_ROOT'], '/') . '/sites/default/files/npo_projects/' . $user->uid . '/')) {
-    mkdir(trim($_SERVER['DOCUMENT_ROOT'], '/') . '/sites/default/files/npo_projects/' . $user->uid . '/', 0777);
+  if (!is_dir(rtrim($_SERVER['DOCUMENT_ROOT'], '/') . '/sites/default/files/npo_projects/' . $user->uid . '/')) {
+    mkdir(rtrim($_SERVER['DOCUMENT_ROOT'], '/') . '/sites/default/files/npo_projects/' . $user->uid . '/', 0777);
   }
 
-  if (!is_dir(trim($_SERVER['DOCUMENT_ROOT'], '/') . '/sites/default/files/npo_projects/' . $user->uid . '/files/')) {
-    mkdir(trim($_SERVER['DOCUMENT_ROOT'], '/') . '/sites/default/files/npo_projects/' . $user->uid . '/files/', 0777);
+  if (!is_dir(rtrim($_SERVER['DOCUMENT_ROOT'], '/') . '/sites/default/files/npo_projects/' . $user->uid . '/files/')) {
+    mkdir(rtrim($_SERVER['DOCUMENT_ROOT'], '/') . '/sites/default/files/npo_projects/' . $user->uid . '/files/', 0777);
   }
 
   $full_path = $file_new_name . $file_name;
@@ -65,6 +65,7 @@ if ($auth) {
 
   } else {
     $error = 'Some error occured please try again later';
+    $error = $full_path;
     $http_path = '';
   }
 } else {
