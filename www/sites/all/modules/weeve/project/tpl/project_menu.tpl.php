c525e212744c030557e7630129ad5c5230e878a9
Alexey Suhih <lexerom@gmail.com>
2012-01-18 14:04:21 +0700

    hotfixes 4,5,6,7

--- www/sites/all/modules/weeve/project/tpl/project_menu.tpl.php
+++ www/sites/all/modules/weeve/project/tpl/project_menu.tpl.php
@@ -4,5 +4,6 @@
   <li><?= l(t('discussion'), 'project/' . $nid . '/comments') ?></li>
   <?php if ($edit): ?>
     <li><?= l(t('edit'), 'project/' . $nid . '/edit') ?></li>
+    <li><?= l(t('cancel'), 'project/' . $nid . '/cancel') ?></li>
   <?php endif; ?>
 </ul>
