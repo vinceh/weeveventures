4c4bca452bd5a933d0a1523d52bb889a874890ef
Alexey Suhih <lexerom@gmail.com>
2012-01-23 14:49:37 +0700

    hotfixes

---
+++ www/sites/all/modules/weeve/project/tpl/proposal_add_element.tpl.php
@@ -0,0 +1,5 @@
+<div class="element-title"><?= t('Tell us about your project') ?></div>
+<div class="element-description">
+  <?= t('Weeve projects are intended to help society. They must have a clear set of goals with well defined milestones. Please be clear and concise'); ?>
+</div>
+<?= drupal_render($element) ?>
