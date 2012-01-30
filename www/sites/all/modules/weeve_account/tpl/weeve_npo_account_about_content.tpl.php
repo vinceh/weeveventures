db2f8c00d2e2e57c636968bdf6750ba969078d42
nshkuro <nikolas.shkuro@gmail.com>
2012-01-30 11:01:31 +0700

    Hotfixes

--- www/sites/all/modules/weeve_account/tpl/weeve_npo_account_about_content.tpl.php
+++ www/sites/all/modules/weeve_account/tpl/weeve_npo_account_about_content.tpl.php
@@ -1,5 +1,11 @@
 <div class='npo-profile-about-wrap'>
+  <?php if($profile->field_npo_about[0]['value']): ?>
   <?php print check_markup($profile->field_npo_about[0]['value']); ?>
+  <?php else: ?>
+  <div>
+    <p><?php print t(''); ?></p>
+  </div>
+  <?php endif; ?>
 </div>
 
 
