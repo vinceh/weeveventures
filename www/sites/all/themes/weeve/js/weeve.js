eb388f342f3add47cf6b660c054204cf75800007
Alexey Suhih <lexerom@gmail.com>
2012-02-01 10:58:40 +0700

    notification box

--- www/sites/all/themes/weeve/js/weeve.js
+++ www/sites/all/themes/weeve/js/weeve.js
@@ -2,4 +2,30 @@ $(document).ready(function() {
   $(window).resize(function() {
     $(".ui-dialog-content").dialog("option", "position", "center");
   });
+
+  $('.weeve-message .message-close').live('click', function(e) {
+    e.preventDefault();
+    $('.weeve-message').fadeOut('slow', function() {
+      $('.weeve-message').remove();
+    });
+    
+  })
 });
+
+function weeve_set_message(message, status) {
+  $('.weeve-message').remove();
+  if (status == undefined) {
+    status = 'status';
+  }
+
+  var output = '<div class="weeve-message messages '+status+'">';
+  output += Drupal.t(message);
+  output += '<a class="message-close" href="#">' + Drupal.t('close')+'</a></div>';
+
+  if ($('#tabs-wrapper').length) {
+    $(output).insertAfter($('#tabs-wrapper'));
+  } else {
+    $('#squeeze .left-corner').prepend(output);
+  }
+  
+}
